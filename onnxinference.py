3a84b5f3402237cb20ee0534c7b64e209a452d85
herpaderpapotato <herpaderpapotato@outlook.com>
2023-11-26 21:26:14 +0800

    new onnx model

--- onnxinference.py
+++ onnxinference.py
@@ -58,7 +58,8 @@ if args.predict:
 sess = None
 
 def download_model():
-    model_url = "https://huggingface.co/herpaderpapotato/sixty_small_body_ryhthm_time_bidirectional/resolve/main/effv2s_60in_60out_130e_1e.onnx?download=true"
+    #model_url = "https://huggingface.co/herpaderpapotato/sixty_small_body_ryhthm_time_bidirectional/resolve/main/effv2s_60in_60out_130e_1e.onnx?download=true"
+    model_url = "https://huggingface.co/herpaderpapotato/sixty_small_body_ryhthm_time_bidirectional_thicc/resolve/main/sixty_small_body_ryhthm_time_bidirectional_thicc_e36.onnx?download=true"
     model_name = model_url.split("/")[-1].split("?")[0]
     model_path = os.path.join("models", model_name)
     if not os.path.exists(model_path):
